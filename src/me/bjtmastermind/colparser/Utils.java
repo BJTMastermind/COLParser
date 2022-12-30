dc57e2a1f2038564b4e14dd5e66931136c112148
BJTMastermind <bjtmastermind@gmail.com>
2022-12-29 20:58:42 -0500

    Merged Assembler and Parser class methods into COLFile class. Renamed and moved ByteUtils class out of utils folder.

--- src/me/bjtmastermind/colparser/Utils.java
+++ src/me/bjtmastermind/colparser/Utils.java
@@ -1,36 +1,36 @@
-package me.bjtmastermind.colparser.util;
+package me.bjtmastermind.colparser;
 
 import java.nio.ByteBuffer;
 import java.util.LinkedList;
 
-public class ByteUtils {
+class Utils {
 
-    public static String bytesToString(byte[] bytes) {
-        return new String(ByteBuffer.wrap(bytes).array());
-    }
-
-    public static void addShortToBuffer(LinkedList<Byte> addTo, short value) {
-        byte[] valueAsBytes = ByteBuffer.allocate(2).putShort(value).array();
-        for (int i = 0; i < valueAsBytes.length; i++) {
-            addTo.add(valueAsBytes[i]);
-        }
-    }
-
-    public static void addIntToBuffer(LinkedList<Byte> addTo, int value) {
+    static void addIntToBuffer(LinkedList<Byte> addTo, int value) {
         byte[] valueAsBytes = ByteBuffer.allocate(4).putInt(value).array();
         for (int i = 0; i < valueAsBytes.length; i++) {
             addTo.add(valueAsBytes[i]);
         }
     }
 
-    public static void addStringToBuffer(LinkedList<Byte> addTo, String value) {
+    static void addShortToBuffer(LinkedList<Byte> addTo, short value) {
+        byte[] valueAsBytes = ByteBuffer.allocate(2).putShort(value).array();
+        for (int i = 0; i < valueAsBytes.length; i++) {
+            addTo.add(valueAsBytes[i]);
+        }
+    }
+
+    static void addStringToBuffer(LinkedList<Byte> addTo, String value) {
         byte[] valueAsBytes = ByteBuffer.allocate(value.length()).put(value.getBytes(), 0, value.length()).array();
         for (int i = 0; i < valueAsBytes.length; i++) {
             addTo.add(valueAsBytes[i]);
         }
     }
 
-    public static byte[] listToArray(LinkedList<Byte> src) {
+    static String bytesToString(byte[] bytes) {
+        return new String(ByteBuffer.wrap(bytes).array());
+    }
+
+    static byte[] listToArray(LinkedList<Byte> src) {
         byte[] output = new byte[src.size()];
         for (int i = 0; i < src.size(); i++) {
             output[i] = src.get(i);
