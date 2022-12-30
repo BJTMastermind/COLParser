492eb3127b8c83ba6eec003382d7019b22544f18
BJTMastermind <bjtmastermind@gmail.com>
2022-12-29 21:03:16 -0500

    Added 2 new methods parsingTest and assemblingTest. functions the same as before but are not their own methods.

--- src/test/bjtmastermind/colparser/Test.java
+++ src/test/bjtmastermind/colparser/Test.java
@@ -1,28 +1,35 @@
 package test.bjtmastermind.colparser;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
-import me.bjtmastermind.colparser.Assembler;
 import me.bjtmastermind.colparser.COLFile;
-import me.bjtmastermind.colparser.Parser;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        Parser parser = new Parser(new File("path/to/colours.col"));
-        COLFile colFile = parser.parse();
+        parsingTest();
+        assemblingTest("path/to/colours.col", "/path/to/new_colors.col");
+    }
 
-        for (String color : colFile.getColors().keySet()) {
-            System.out.println(color+" | "+COLFile.toHex(colFile.getColors().get(color)));
-        }
-        for (String color : colFile.getWaterColors().keySet()) {
-            System.out.print(color+" | ");
-            for (int i = 0; i < 3; i++) {
-                System.out.print(COLFile.toHex(colFile.getWaterColors().get(color)[i])+" ");
-            }
-            System.out.println("");
-        }
+    private static void parsingTest() throws IOException {
+        COLFile colFile = new COLFile();
+        colFile.parse("path/to/colours.col");
 
-        Assembler assembler = new Assembler(colFile);
-        assembler.assemble("path/to/new_colours.col");
+        System.out.println(colFile.printColors());
+        System.out.println(colFile.printWaterColors());
+    }
+
+    private static void assemblingTest(String intputPath, String outputPath) throws IOException {
+        COLFile colFile = new COLFile();
+        colFile.parse(intputPath);
+
+        colFile.assemble(outputPath);
+
+        if (Files.exists(new File(outputPath).toPath())) {
+            System.out.println("Successfully created file at: '"+outputPath+"'.");
+        } else {
+            System.err.println("Failed to create file.");
+        }
     }
 }
