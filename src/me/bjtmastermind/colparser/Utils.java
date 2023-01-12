867a1c7d467c5bdb97ec666020e5ba642d3113fc
BJTMastermind <bjtmastermind@gmail.com>
2023-01-11 20:47:29 -0500

    Removed now unused methods and modified existing ones a bit.

--- src/me/bjtmastermind/colparser/Utils.java
+++ src/me/bjtmastermind/colparser/Utils.java
@@ -1,40 +1,24 @@
 package me.bjtmastermind.colparser;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.LinkedList;
 
 class Utils {
 
-    static void addIntToBuffer(LinkedList<Byte> addTo, int value) {
-        byte[] valueAsBytes = ByteBuffer.allocate(4).putInt(value).array();
-        for (int i = 0; i < valueAsBytes.length; i++) {
-            addTo.add(valueAsBytes[i]);
-        }
-    }
-
-    static void addShortToBuffer(LinkedList<Byte> addTo, short value) {
-        byte[] valueAsBytes = ByteBuffer.allocate(2).putShort(value).array();
-        for (int i = 0; i < valueAsBytes.length; i++) {
-            addTo.add(valueAsBytes[i]);
-        }
-    }
-
-    static void addStringToBuffer(LinkedList<Byte> addTo, String value) {
-        byte[] valueAsBytes = ByteBuffer.allocate(value.length()).put(value.getBytes(), 0, value.length()).array();
-        for (int i = 0; i < valueAsBytes.length; i++) {
-            addTo.add(valueAsBytes[i]);
-        }
-    }
-
-    static String bytesToString(byte[] bytes) {
-        return new String(ByteBuffer.wrap(bytes).array());
-    }
-
-    static byte[] listToArray(LinkedList<Byte> src) {
+    static byte[] dataOutputStreamToArray(DataOutputStream src) {
         byte[] output = new byte[src.size()];
-        for (int i = 0; i < src.size(); i++) {
-            output[i] = src.get(i);
+        try {
+            src.write(output);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return output;
+    }
+
+    static String getString(ByteBuffer buffer, int size) {
+        byte[] bytes = new byte[size];
+        buffer.get(bytes, 0, size);
+        return new String(bytes);
     }
 }
